23884b7e1080aaf24d6e85e9b6f9494c5aff159f
Umesh Mehta <120069900+UmeshMehta1@users.noreply.github.com>
2023-08-21 21:38:47 +0545

    Counter_01

--- 02CounterProject/src/App.jsx
+++ 02CounterProject/src/App.jsx
@@ -6,28 +6,27 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const addClick = () => {
+    if(count<20){
+      setCount(count + 1)
+    }
+    
+  }
+  const minsClick=()=>{
+    if(count>0){
+      setCount(count-1);
+    }
+   
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Counter</h1>
+      <h1>{count}</h1>
+
+      <button onClick={addClick}>Add</button>
+      <button onClick={minsClick}>Minus</button>
+        
     </>
   )
 }
