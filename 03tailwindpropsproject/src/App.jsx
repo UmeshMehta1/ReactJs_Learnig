8d132f17d9a189a965568e32419e33bc735a1cc3
Umesh Mehta <120069900+UmeshMehta1@users.noreply.github.com>
2023-08-25 20:07:25 +0545

    1

--- 03tailwindpropsproject/src/App.jsx
+++ 03tailwindpropsproject/src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import Cards from './Components/cards'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
  
   return (
--- 03tailwindpropsproject/src/Components/cards.jsx
+++ 03tailwindpropsproject/src/Components/cards.jsx
@@ -1,6 +1,4 @@
-import React from 'react'
-
-export default function Cards(props) { //Also can use distructing
+export default function Cards(props) { //Also can use distructing like ({username, skill})
     // console.log(`username=${username}` )
     // console.log(`skill=${skill} `)
   return (
@@ -66,7 +64,7 @@ export default function Cards(props) { //Also can use distructing
       </div>
       <button className="flex-none flex items-center justify-center w-9 h-9 rounded-md text-slate-300 border border-slate-200" type="button" aria-label="Like">
         <svg width="20" height="20" fill="currentColor" aria-hidden="true">
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
+          <path  d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
         </svg>
       </button>
     </div>
